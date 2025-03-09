95253b137cbccef7c76e0630a4a42b4a4f62b282
GyanRibeiro <gyanribeiro29@gmail.com>
2025-03-09 18:42:37 -0300

    Initial commit - structured project

---
+++ src/test/java/com/desafiotecnico/eclipsehotel/EclipseHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotecnico.eclipsehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
