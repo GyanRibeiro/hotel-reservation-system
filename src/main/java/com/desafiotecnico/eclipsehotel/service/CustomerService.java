488bc152234d66c4b1eeb0092c87bbec97f752c7
GyanRibeiro <gyanribeiro29@gmail.com>
2025-03-09 23:00:33 -0300

    feat: Create CustomerService - Adds the CustomerService class with methods for customer CRUD

---
+++ src/main/java/com/desafiotecnico/eclipsehotel/service/CustomerService.java
@@ -0,0 +1,38 @@
+package com.desafiotecnico.eclipsehotel.service;
+
+import com.desafiotecnico.eclipsehotel.entity.Customer;
+import com.desafiotecnico.eclipsehotel.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerById(Integer id) {
+        return customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(Integer id, Customer customerDetails) {
+        Customer customer = getCustomerById(id);
+        customer.setName(customerDetails.getName());
+        customer.setEmail(customerDetails.getEmail());
+        customer.setPhone(customerDetails.getPhone());
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Integer id) {
+        customerRepository.deleteById(id);
+    }
+}
